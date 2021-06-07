8247a4c67246a5fc16278661fe24ea2b84bdb9f1
Sven Kortekaas <svenkortekaas04@gmail.com>
2021-06-07 20:43:56 +0200

    New function for making API requests

--- apps/frontend/static/frontend/js/index.js
+++ apps/frontend/static/frontend/js/index.js
@@ -4,3 +4,51 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+  // these HTTP methods do not require CSRF protection
+  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function makeRequest(url, method, body={}) {
+  if (csrfSafeMethod(method)) {
+    var response = fetch(
+      url,
+      {
+        method: method,
+        headers: {
+          "Content-Type": "application/json",
+        }
+      }
+    )
+  } else {
+    var response = fetch(
+      url,
+      {
+        body: body,
+        method: method,
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": getCookie('csrftoken')  // Get the CSRF token
+        }
+      }
+    )
+  }
+  return response;
+}
