e6ee8e0fc441a4dab7e4ef2219ebe74c4bb724e0
Sven Kortekaas <svenkortekaas04@gmail.com>
2021-06-07 20:44:31 +0200

    Assign names to the url paths

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,6 +4,6 @@ from apps.api import views
 
 
 urlpatterns = [
-    path("users/", views.UsersView.as_view()),
-    path("users/<str:username>", views.UserView.as_view())
+    path("users/", views.UsersView.as_view(), name="api_users"),
+    path("users/<str:username>", views.UserView.as_view(), name="api_users_user")
 ]
