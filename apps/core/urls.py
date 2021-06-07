aa75094aa58d36c8a6c57cd59de7fe9fa33a09bd
Sven Kortekaas <svenkortekaas04@gmail.com>
2021-06-07 20:44:57 +0200

    Redirect to start page

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,7 +5,7 @@ from apps.core import views
 
 
 urlpatterns = [
-    path("", RedirectView.as_view(pattern_name="core_recent"), name="core_index"),
+    path("", RedirectView.as_view(pattern_name="core_start"), name="core_index"),
     re_path(r"browse(?:/(?P<path>.*))?/$", views.browse, name="core_browse"),
     path("recent/", views.recent, name="core_recent"),
     path("start/", views.start, name="core_start")
