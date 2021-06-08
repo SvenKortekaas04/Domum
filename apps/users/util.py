4945170ee906a7b654b994180fd149b14b4e7e31
Sven Kortekaas <svenkortekaas04@gmail.com>
2021-06-08 16:35:33 +0200

    Changes: - New 'delete_user' function - Dynamically get user model

--- apps/users/util.py
+++ apps/users/util.py
@@ -1,12 +1,14 @@
 import os
 import secrets
+import shutil
 from typing import (
     Tuple
 )
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
-from apps.users.models import Settings, User
+from apps.users.models import Settings
 
 
 def create_storage_id() -> str:
@@ -31,12 +33,13 @@ def init_user_storage() -> Tuple[str]:
     return storage_path, storage_id
 
 
-def init_user(username: str, first_name: str, last_name: str, email: str, password: str) -> User:
+def init_user(username: str, first_name: str, last_name: str, email: str, password: str):
     """Initialize a new user."""
 
     # Initialize storage
     storage_path, storage_id = init_user_storage()
 
+    User = get_user_model()
     user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password, storage_path=storage_path, storage_id=storage_id)
 
     # Assign user specific settings
@@ -44,3 +47,14 @@ def init_user(username: str, first_name: str, last_name: str, email: str, passwo
     settings.save()
 
     return user
+
+
+def delete_user(username: str) -> None:
+    """Delete a user."""
+
+    User = get_user_model()
+    user = User.objects.get(username=username)
+    user.delete()
+
+    # Delete storage
+    shutil.rmtree(user.storage_path)
